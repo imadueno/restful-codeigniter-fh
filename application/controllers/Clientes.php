4b0a88b153ab69ab28df4e3d223f3e71f9f3c9c4
madueno <isai.madueno.23@gmail.com>
2019-10-29 17:51:20 -0700

    servicio cliente agregado

--- application/controllers/Clientes.php
+++ application/controllers/Clientes.php
@@ -1,14 +1,45 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 require( APPPATH.'/libraries/REST_Controller.php');
-use Restserver\libraries\REST_Controller;
 
 class Clientes extends REST_Controller {
 
     public function __construct(){
         parent::__construct();
         $this->load->database();
+        $this->load->model('Cliente_model');
     }
 
-    // Servicios
+    public function cliente_get(){
+        $cliente_id = $this->uri->segment(3);
+
+        // validar cliente id
+        if(!isset($cliente_id)){
+            $respuesta = array(
+                'error' => TRUE,
+                'mensaje' => 'ID no proporcionado'
+                );
+            $this->response($respuesta , REST_Controller::HTTP_BAD_REQUEST);
+            return;
+        }
+
+        $cliente = $this->Cliente_model->get_cliente($cliente_id);
+        if(isset($cliente)){
+            $respuesta = array(
+                'error' => TRUE,
+                'mensaje' => 'ID no proporcionado',
+                'cliente' => $cliente
+            );
+            $this->response($respuesta);
+        }else{
+            $respuesta = array(
+                'error' => TRUE,
+                'mensaje' => 'ID '.$cliente_id.' no existe.',
+                'cliente' => $cliente
+            );
+            $this->response($respuesta , REST_Controller::HTTP_NOT_FOUND);
+        }
+        $this->response($cliente);
+    }
 }
---
+++ application/models/Cliente_model.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Cliente_model extends CI_Model {
+
+    // Campos de la tabla clientes
+    public $id;
+    public $nombre;
+    public $activo;
+    public $zip;
+    public $telefono1;
+    public $telefono2;
+    public $pais;
+    public $direccion;
+
+    public function get_cliente( $id ){
+        $this->db->where(array('id' => $id,'activo' => 1));
+        $query = $this->db->get('clientes');
+        $row = $query->custom_row_object(0,'Cliente_model');
+        return $row;
+    }
+
+}
